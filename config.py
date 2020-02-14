ff2578d2139e6b06df26461d56f8b89f978163c7
Nezra <96aa07@gmail.com>
2020-02-14 10:10:49 -0500

    initial.

---
+++ config.py
@@ -0,0 +1,9 @@
+from ui import colours
+# global config
+UI_PLACEMENT_MODE = False
+RESOLUTION = (1920, 1080)
+# RESOLUTION = (1366, 768)
+FPS = 60
+DEV_MODE = True
+SOUND = True
+RACOLOUR = colours.RED5
